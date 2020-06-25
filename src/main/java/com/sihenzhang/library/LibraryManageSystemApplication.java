a4105ba91379e77f81b18287129ba116a8f082fd
SihenZhang <1334125013@qq.com>
2020-06-25 13:59:18 +0800

    Initial commit

---
+++ src/main/java/com/sihenzhang/library/LibraryManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sihenzhang.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManageSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sihenzhang/library/LibraryManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihenzhang.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
